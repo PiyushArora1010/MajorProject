7f158603859d7ddb224c11d50c4aefca461aa5ff
Piyush Arora <arora.8@iitj.ac.in>
2024-05-01 19:17:05 +0530

    changes

--- module/eval.py
+++ module/eval.py
@@ -12,4 +12,27 @@ def accuracy(loader, model, device):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+    model.train()
     return correct / total
+
+def accuracyCustom(loader, memloader, model, device):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data, mem in zip(loader, memloader):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            mem, _ = mem
+            mem = mem.to(device)
+            outputs = model(inputs, mem)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    model.train()
+    return correct / total
+
+evalDic = {
+    'accuracy': accuracy,
+    'accuracyCustom': accuracyCustom
+}
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import torch
 
 from torch.utils.data import random_split
 
-from module.eval import accuracy
+from module.eval import evalDic
 from module.models import modelDic
 
 from utils import UrbanSound, ESC50Dataset
@@ -18,6 +18,8 @@ class trainer:
         
         if not os.path.exists('models'):
             os.makedirs('models')
+        
+        self.model_path = 'models/'+self.run_name+'.pt'
 
 
     def _setDataset(self):
@@ -39,6 +41,9 @@ class trainer:
         self.validloader = torch.utils.data.DataLoader(validdataset, batch_size=self.batch_size, num_workers = self.workers, shuffle=False)
         self.testloader = torch.utils.data.DataLoader(testdataset, batch_size=self.batch_size, num_workers = self.workers, shuffle=False)
 
+        if self.model_name != 'astmodel':
+            self.memloader = torch.utils.data.DataLoader(traindataset, batch_size=self.batch_size, num_workers = self.workers, shuffle=True)
+
     def _setModelAndOptimizer(self):
         self.model = modelDic[self.model_name](self.classes).to(DEVICE)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
@@ -47,6 +52,7 @@ class trainer:
         self.loss_fn = torch.nn.CrossEntropyLoss()
 
     def _train(self):
+        accuracy = evalDic['accuracy']
         bestAcc = 0
         for epoch in range(self.epochs):
             self.model.train()
@@ -64,16 +70,52 @@ class trainer:
             print(f'Epoch: {epoch+1}, Training Accuracy: {trainAcc} Validation Accuracy: {validAcc}')
             if validAcc > bestAcc:
                 bestAcc = validAcc
-                torch.save(self.model.state_dict(), 'models/'+self.run_name+'.pt')
+                torch.save(self.model.state_dict(), self.model_path)
 
     def _test(self):
+        accuracy = evalDic['accuracy']
         self.model.load_state_dict(torch.load(self.model_path))
         testAcc = accuracy(self.testloader, self.model, DEVICE)
+        print(f'Test Accuracy: {testAcc}')
+
+    def _trainCustom(self):
+        accuracy = evalDic['accuracycustom']
+        bestAcc = 0
+        for epoch in range(self.epochs):
+            self.model.train()
+            for data, mem in zip(self.trainloader, self.memloader):
+                inputs, labels = data
+                inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
+
+                mem, _ = mem
+                mem = mem.to(DEVICE)
+
+                self.optimizer.zero_grad()
+                outputs = self.model(inputs, mem)
+                loss = self.loss_fn(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+            trainAcc = accuracy(self.trainloader, self.memloader, self.model, DEVICE)
+            validAcc = accuracy(self.validloader, self.memloader, self.model, DEVICE)
+            print(f'Epoch: {epoch+1}, Training Accuracy: {trainAcc} Validation Accuracy: {validAcc}')
+            if validAcc > bestAcc:
+                bestAcc = validAcc
+                torch.save(self.model.state_dict(), self.model_path)
+        
+    def _testCustom(self):
+        accuracy = evalDic['accuracycustom']
+        self.model.load_state_dict(torch.load(self.model_path))
+        testAcc = accuracy(self.testloader, self.memloader, self.model, DEVICE)
         print(f'Test Accuracy: {testAcc}')
 
     def run(self):
         self._setDataset()
         self._setModelAndOptimizer()
         self._setLossFunction()
-        self._train()
-        self._test()
+        if self.model_name == 'astmodel':
+            self._train()
+            self._test()
+        else:
+            self._trainCustom()
+            self._testCustom()
