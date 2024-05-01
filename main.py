bdc751c7b3491bb2121ae117cd8dac30102b82cf
Piyush Arora <arora.8@iitj.ac.in>
2024-05-01 21:32:15 +0530

    changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 
+print(os.system('nvidia-smi'))
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 from train import trainer
@@ -10,9 +11,9 @@ parser = argparse.ArgumentParser(description='Audio Classification')
 
 parser.add_argument('--dataset', type=str, default='urbansound', help='Dataset to use')
 parser.add_argument('--model_name', type=str, default='astmodel', help='Model to use')
-parser.add_argument('--epochs', type=int, default=10, help='Number of epochs')
+parser.add_argument('--epochs', type=int, default=20, help='Number of epochs')
 parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
-parser.add_argument('--lr', type=float, default=3e-4, help='Learning rate')
+parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate')
 parser.add_argument('--weight_decay', type=float, default=1e-5, help='Weight decay')
 parser.add_argument('--workers', type=int, default=4, help='Number of workers')
 parser.add_argument('--targetLength', type=int, default=1024, help='Target length of audio')
